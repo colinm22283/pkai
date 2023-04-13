797c0a16596cd4bb921d0237c255b582ccb7805f
Colin M <colinm22@iastate.edu>
2023-04-13 15:15:55 -0500

    Initial commit for version 2

--- custom/source/main.cpp
+++ custom/source/main.cpp
@@ -2,8 +2,7 @@
 #include <chrono>
 #include <fstream>
 
-#include "pkai/network.hpp"
-#include "pkai/training_set.hpp"
+#include <cuda_runtime_api.h>
 
 constexpr unsigned long training_iterations = 51000;
 constexpr const char * network_path = "networks/cifar10.net";
@@ -25,65 +24,7 @@ int main() {
     cudaDeviceSynchronize();
     check_cuda_error();
 
-    std::cout << "Loading network...\n";
-    PKAI::Network network(network_path);
-    check_cuda_error();
 
-    std::cout << "Loading training set...\n";
-    PKAI::TrainingSet training_set(dataset_path);
-    check_cuda_error();
-
-    std::cout << "Training...\n";
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-    network.run(training_set, training_iterations, 1000, [](PKAI::Network & network, unsigned long iteration) {
-        std::cout << iteration << '/' << training_iterations << '\n';
-        std::cout << "Saving network...\n";
-        network.save(network_path);
-    });
-    auto end_time = std::chrono::high_resolution_clock::now();
-    check_cuda_error();
-
-    unsigned long elapsed = (end_time - start_time).count();
-
-    std::cout << "Training complete\n";
-    printf(
-        "Elapsed time: %2.lu:%2.lu:%2.3f\n\n",
-        elapsed / 360000000000 % 60,
-        elapsed / 60000000000 % 60,
-        fmodf32((float) elapsed / 1000000000.0f, 60)
-    );
-    std::cout << "Press enter to continue...\n";
-    std::cin.get();
-
-    network.run(training_set, 50, [](PKAI::Network & network, PKAI::training_pair_t & current_pair, unsigned long iteration) {
-//        network.print();
-        float temp[network.layer_sizes[network.layer_count - 1]];
-
-        network.extract_outputs(temp);
-
-        std::cout << "Output: [ ";
-        for (unsigned long i = 0; i < network.layer_sizes[network.layer_count - 1]; i++) {
-            std::cout << temp[i] << " ";
-        }
-        std::cout << "]\n";
-
-        current_pair.extract_outputs(temp, network.layer_sizes[network.layer_count - 1]);
-
-        std::cout << "Correct: [ ";
-        for (unsigned long i = 0; i < network.layer_sizes[network.layer_count - 1]; i++) {
-            std::cout << temp[i] << " ";
-        }
-        std::cout << "]\n";
-
-    });
-    check_cuda_error();
-
-    std::cout << "Saving network...\n";
-    network.save(network_path);
-    check_cuda_error();
-
-    std::cout << "Done!\n";
 
     return 0;
 }
